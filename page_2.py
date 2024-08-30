f75821bf1ad899cc2dd6366cf005b287e9227811
luidgicolimerio <luidgicolimerio@gmail.com>
2024-08-30 10:38:16 -0300

    fix duplicates and add description for cnab

--- page_2.py
+++ page_2.py
@@ -6,7 +6,7 @@ df_empresas = pd.read_pickle('assets/df_empresa.pkl')
 df_empresas_valores = pd.read_pickle('assets/df_empresas_valores.pkl')
 df_valor_total = pd.read_pickle('assets/df_valor_total.pkl')
 df_empresa_cnab = pd.read_pickle('assets/df_empresa_cnab.pkl')
-df_cnab = pd.read_pickle('assets/df_cnab.pkl')
+df_cnab = pd.read_pickle('assets/df_cnab_desc.pkl')
 df_empresa_cnab_valor = pd.read_pickle('assets/df_empresa_cnab_valor.pkl')
 
 # Pagina com filtros
@@ -17,15 +17,14 @@ def consulta(nome):
     return df_valor, df_transacoes
 
 def consulta_cnab(cnab, nome):
-
-    df_1 = df_empresa_cnab_valor[df_empresa_cnab_valor['CNAB'] == cnab]
+    df_1 = df_empresa_cnab_valor[df_empresa_cnab_valor['CNAB'] == int(cnab[:3])]
     df_transacao = df_1[df_1['NOME_PESSOA_OD'] == nome].drop('NOME_PESSOA_OD', axis=1)
 
     return df_transacao
 
 
 opcoes_empresa = df_empresas['NOME_PESSOA_OD'].unique()
-opcoes_cnab = df_cnab['CNAB'].unique()
+opcoes_cnab = df_cnab['descricao'].unique()
 
 escolha_empresa = st.selectbox("Escolha alguem para investigar: ", opcoes_empresa)
 
