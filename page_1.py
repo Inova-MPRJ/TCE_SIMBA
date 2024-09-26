1015505c92955f51a34f0f301cb7dfb5f676308b
luidgicolimerio <luidgicolimerio@gmail.com>
2024-09-26 17:58:48 -0300

    modularization

--- page_1.py
+++ page_1.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import pandas as pd
+from data import empresas, maiores
 
-df_empresas = pd.read_pickle('assets/df_empresa.pkl')
 df_empresas_valores = pd.read_pickle('assets/df_empresas_valores.pkl')
-df_maiores = pd.read_pickle('assets/df_maiores_reset.pkl')
-
+df_filtrado = pd.read_pickle('assets/df_filtrado.pkl')
 # Pagina de Teste Inicial
 
-st.title("Estes são os dados do navega")
+st.title("Visão geral do município")
 
 st.write('Todas as empresas/pessoas que receberam:')
-st.dataframe(df_empresas)
+st.dataframe(empresas(df_filtrado))
 st.write('Todas as transações:')
 st.dataframe(df_empresas_valores)
 st.write('As empresas/pessoas que mais receberam:')
+df_maiores = maiores(df_filtrado, 29111093000103, 29111622000179, 11835031000189, 1193480000117, 2098399000110, 42498675000152, 13499878000165, 60746948000112) 
 st.dataframe(df_maiores)
 st.bar_chart(df_maiores.set_index('Empresa'))
